63dc380fc802b244dae92429836d2503972edb97
Thomas Dau <ThomasQDau@gmail.com>
2019-04-25 19:41:07 -0700

    xfering to desktop

--- models/messages.js
+++ models/messages.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
     var Message = sequelize.define("Message", {
       body: {
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(150),
         allowNull: false,
         validate: {
           len: [1,150]
--- server.js
+++ server.js
@@ -5,11 +5,15 @@ var db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+var routes = require("./routes/api-routes.js");
+app.use(routes);
 
 // TODO: Make routes later
 // require("./routes/html-routes.js")(app);
 // require("./routes/author-api-routes.js")(app);
 // require("./routes/post-api-routes.js")(app);
+
+
 
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
