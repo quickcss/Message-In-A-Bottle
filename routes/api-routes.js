76db0b472eb1d994a6fc5b0c08b4f8d6e2fb11a0
Thomas Dau <ThomasQDau@gmail.com>
2019-04-29 10:44:22 -0700

    created random generating bottle array

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,16 +3,42 @@ var router = express.Router();
 var db = require("../models");
 
 router.get("/", function (req, res) {
-    db.Messages.findAll({}).then(function(data) {
-        res.render("index", {Messages: data});
+    db.Messages.count().then(function (bottlesNumber) {
+        var randomBottlesArr = [];
+        var bottles = 5;
+        if (bottlesNumber < 5) {
+            bottles = bottlesNumber;
+        }
+        for (var i = 0; i < bottles; i++) {
+            function randomNumGen () {
+                var randomNum = Math.floor(Math.random() * bottlesNumber) + 1
+                if (randomBottlesArr.includes(randomNum)) {
+                    randomNumGen();
+                } else {
+                    randomBottlesArr.push(randomNum);
+                }
+            }
+            randomNumGen();
+        }
+        db.Messages.findAll({
+            where: {
+                id: randomBottlesArr
+            }
+        }).then(function (data) {
+            console.log(bottlesNumber);
+            console.log(data);
+            res.render("index", { Messages: data });
+        })
     })
 });
 
 router.post("/api/message", function (req, res) {
     console.log(req.body);
-    db.Messages.create(req.body).then(function(data){
+    db.Messages.create(req.body).then(function (data) {
         res.end();
-    })
-})
+    });
+});
+
+
 
 module.exports = router;
